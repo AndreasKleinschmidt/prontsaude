d3bd04d0d041e599f499fd7195968c47aebbfa0b
AndreasKleinschmidt <166660081+AndreasKleinschmidt@users.noreply.github.com>
2024-04-12 09:31:15 -0300

    Add files via upload

---
+++ login/login_form_user.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title><?php echo $this->Ini->Nm_lang['lang_login_message_16']; ?></title>
+		<meta name="description" content="<?php echo $this->Ini->Nm_lang['lang_login_message_7']; ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="../_lib/libraries/grp/healthcare/main.css">
+		 <link rel="stylesheet" href="<?php echo $this->Ini->path_prod ?>/third/jquery_plugin/thickbox/thickbox.css" type="text/css" media="screen" />
+ <SCRIPT type="text/javascript">
+  var sc_pathToTB = '<?php echo $this->Ini->path_prod ?>/third/jquery_plugin/thickbox/';
+  var sc_tbLangClose = "<?php echo html_entity_decode($this->Ini->Nm_lang["lang_tb_close"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]) ?>";
+  var sc_tbLangEsc = "<?php echo html_entity_decode($this->Ini->Nm_lang["lang_tb_esc"], ENT_COMPAT, $_SESSION["scriptcase"]["charset"]) ?>";
+  var sc_userSweetAlertDisplayed = false;
+ </SCRIPT>
+        <SCRIPT type="text/javascript" src="<?php echo $this->Ini->url_third; ?>jquery/js/jquery.js"></SCRIPT>
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->path_prod; ?>/third/jquery/js/jquery-ui.js"></SCRIPT>
+ <link rel="stylesheet" href="<?php echo $this->Ini->path_prod ?>/third/jquery/css/smoothness/jquery-ui.css" type="text/css" media="screen" />
+ <link rel="stylesheet" type="text/css" href="<?php echo $this->Ini->path_link ?>_lib/css/<?php echo $this->Ini->str_schema_all ?>_sweetalert.css" />
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->path_prod; ?>/third/sweetalert/sweetalert2.all.min.js"></SCRIPT>
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->path_prod; ?>/third/sweetalert/polyfill.min.js"></SCRIPT>
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->path_prod; ?>/third/jquery_plugin/thickbox/thickbox-compressed.js"></SCRIPT>
+<link rel="stylesheet" href="<?php echo $this->Ini->path_prod ?>/third/jquery_plugin/select2/css/select2.min.css" type="text/css" />
+<script type="text/javascript" src="<?php echo $this->Ini->path_prod ?>/third/jquery_plugin/select2/js/select2.full.min.js"></script>
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->url_lib_js; ?>scInput.js"></SCRIPT>
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->url_lib_js; ?>jquery.scInput.js"></SCRIPT>
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->url_lib_js; ?>jquery.scInput2.js"></SCRIPT>
+ <SCRIPT type="text/javascript" src="<?php echo $this->Ini->url_lib_js; ?>jquery.fieldSelection.js"></SCRIPT>
+ <link rel="stylesheet" type="text/css" href="<?php echo $this->Ini->path_prod; ?>/third/font-awesome/6/css/all.min.css" />
+
+<script>
+var scFocusFirstErrorField = true;
+var scFocusFirstErrorName  = "<?php if (isset($this->scFormFocusErrorName)) {echo $this->scFormFocusErrorName;} ?>";
+</script>
+
+<?php
+include_once("login_sajax_js.php");
+?>
+<script type="text/javascript">
+var Nm_Proc_Atualiz = false;
+</script>
+<script type="text/javascript">
+<?php
+
+include_once('login_jquery.php');
+
+?>
+</script>
+<script type="text/javascript">
+ $(function() {
+  scJQElementsAdd('');
+  scJQGeneralAdd();
+ });
+
+</script>
+<script type="text/javascript">
+function NM_tp_critica(TP)
+{
+    if (TP == 0 || TP == 1 || TP == 2)
+    {
+        nmdg_tipo_crit = TP;
+    }
+}
+</script> 
+<?php
+ include_once("login_js0.php");
+?>
+<script type="text/javascript"> function setLocale(oSel)
+ {
+  var sLocale = "";
+  if (-1 < oSel.selectedIndex)
+  {
+   sLocale = oSel.options[oSel.selectedIndex].value;
+  }
+  document.F1.nmgp_idioma_novo.value = sLocale;
+ }
+</script><?php
+$msgDefClose = isset($this->arr_buttons['bmessageclose']) ? $this->arr_buttons['bmessageclose']['value'] : 'Ok';
+?>
+<script type="text/javascript">
+var scMsgDefTitle = "<?php if (isset($this->Ini->Nm_lang['lang_usr_lang_othr_msgs_titl'])) {echo $this->Ini->Nm_lang['lang_usr_lang_othr_msgs_titl'];} ?>";
+var scMsgDefButton = "Ok";
+var scMsgDefClose = "<?php echo $msgDefClose; ?>";
+var scMsgDefClick = "close";
+var scMsgDefScInit = "<?php echo $this->Ini->sc_page; ?>";
+</script>
+<script type='text/javascript'>
+bLigEditLookupCall = <?php if ($this->lig_edit_lookup_call) { ?>true<?php } else { ?>false<?php } ?>;
+function scLigEditLookupCall()
+{
+<?php
+if ($this->lig_edit_lookup && isset($_SESSION['sc_session'][$this->Ini->sc_page]['login']['sc_modal']) && $_SESSION['sc_session'][$this->Ini->sc_page]['login']['sc_modal'])
+{
+?>
+  parent.<?php echo $this->lig_edit_lookup_cb; ?>(<?php echo $this->lig_edit_lookup_row; ?>);
+<?php
+}
+elseif ($this->lig_edit_lookup)
+{
+?>
+  opener.<?php echo $this->lig_edit_lookup_cb; ?>(<?php echo $this->lig_edit_lookup_row; ?>);
+<?php
+}
+?>
+}
+if (bLigEditLookupCall)
+{
+  scLigEditLookupCall();
+}
+<?php
+if (isset($this->redir_modal) && !empty($this->redir_modal))
+{
+    echo $this->redir_modal;
+}
+?>
+</script>
+<?php
+$sIconTitle = '' != $this->Ini->Err_ico_title ? $this->Ini->path_icones . '/' . $this->Ini->Err_ico_title : '';
+$sErrorIcon = (isset($_SESSION['scriptcase']['error_icon']['app_Login']) && '' != $_SESSION['scriptcase']['error_icon']['app_Login']) ? $_SESSION['scriptcase']['error_icon']['app_Login']  : "";
+$sCloseIcon = (isset($_SESSION['scriptcase']['error_close']['app_Login']) && '' != trim($_SESSION['scriptcase']['error_close']['app_Login'])) ? $_SESSION['scriptcase']['error_close']['app_Login'] : "<td><input class=\"scButton_default\" type=\"button\" onClick=\"document.getElementById('id_error_display_fixed').style.display = 'none'; document.getElementById('id_error_message_fixed').innerHTML = ''; return false\" value=\"X\" /></td>";
+if ('' != $sIconTitle && '' != $sErrorIcon) {
+    $sErrorIcon = '';
+}
+?>
+<script type="text/javascript">
+$(function() {
+    $(document.F1).on("submit", function (e) {
+        e.preventDefault();
+    });
+});
+
+if (typeof scDisplayUserError === "undefined") {
+    function scDisplayUserError(errorMessage) {
+        scJs_alert("ERROR:\r\n" + errorMessage.replace(/<br \/>/gi, "<!--SC_NL-->"), function() {}, {type: "error"});
+    }
+}
+if (typeof scDisplayUserDebug === "undefined") {
+    function scDisplayUserDebug(debugMessage) {
+        scJs_alert("DEBUG:\r\n" + debugMessage.replace(/<br \/>/gi, "<!--SC_NL-->"), function() {}, {type: "warning"});
+    }
+}
+if (typeof scDisplayUserMessage === "undefined") {
+    function scDisplayUserMessage(userMessage) {
+        scJs_alert("MESSAGE:\r\n" + userMessage.replace(/<br \/>/gi, "<!--SC_NL-->"), function() {}, {type: "info"});
+    }
+}
+</script>
+
+<script>
+$(function() {
+<?php
+if (isset($this->nmgp_cmp_hidden) && !empty($this->nmgp_cmp_hidden)) {
+    foreach($this->nmgp_cmp_hidden as $fieldDisplayFieldName => $fieldDisplayFieldStatus) {
+        if ('on' == $fieldDisplayFieldStatus) {
+?>
+if (typeof scShowUserField === "function") {
+    scShowUserField("<?php echo $fieldDisplayFieldName ?>");
+}
+<?php
+        }
+        if ('off' == $fieldDisplayFieldStatus) {
+?>
+if (typeof scHideUserField === "function") {
+    scHideUserField("<?php echo $fieldDisplayFieldName ?>");
+}
+<?php
+        }
+    }
+}
+?>
+});
+</script>
+
+        <META http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['scriptcase']['charset_html'] ?>" />
+	
+		<?php
+			if(isset($_GET['lang']) && !empty($_GET['lang'])) {
+				$idioma = $_GET['lang'];
+			} else {
+				$idioma = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
+			}
+		?>
+	
+		<style>
+			.flags {
+				border: 1px solid white;
+				padding: 0px!important;
+				display: inline-block!important;
+				height: 22px!important;				
+			}
+			
+			.flags:hover {
+				border-color: #ddd!important;
+			}
+
+			.flag-<?php echo $idioma ?>,
+			.flag-<?php echo $idioma ?>:hover {
+				border-color: #439894!important;
+    			background: #f1f1f1;
+    			cursor: default;
+			}
+        span.sc-ui-pwd-toggle-icon {
+            position: relative;
+            display: inline-block;
+            cursor: pointer;
+            right: 0.5rem;
+            z-index: 2;
+            float: right;
+            bottom: 1.7rem;
+        }
+		</style>
+	</head>
+
+	<body class="page-background" style="background-image:url('../_lib/libraries/grp/healthcare/healthcare-bg.jpg');">
+		<div class="page">
+			<div class="container">
+				<div class="grid">
+					<div class="col-7">
+						<div class="content">
+							<h1>
+								<?php echo $this->Ini->Nm_lang['lang_login_titulo']; ?>
+							</h1>
+
+							<p>
+								<?php echo $this->Ini->Nm_lang['lang_login_desc']; ?>
+							</p>
+							<p>
+								<?php echo $this->Ini->Nm_lang['lang_page_button_download']; ?>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-1">
+						&nbsp;
+					</div>
+						
+					<div class="col-4 box-form">
+						<form class="form" action=""  name="F1" method="post" 
+               action="./" 
+               target="_self">
+                          <input type="hidden" name="nm_form_submit" value="1">
+<input type="hidden" name="nmgp_idioma_novo" value="">
+<input type="hidden" name="nmgp_schema_f" value="">
+<input type="hidden" name="nmgp_url_saida" value="<?php echo $this->form_encode_input($nmgp_url_saida); ?>">
+<input type="hidden" name="bok" value="OK">
+<input type="hidden" name="nmgp_opcao" value="">
+<input type="hidden" name="nmgp_ancora" value="">
+<input type="hidden" name="nmgp_num_form" value="<?php  echo $this->form_encode_input($nmgp_num_form); ?>">
+<input type="hidden" name="nmgp_parms" value="">
+<input type="hidden" name="script_case_init" value="<?php  echo $this->form_encode_input($this->Ini->sc_page); ?>">
+<input type="hidden" name="NM_cancel_return_new" value="<?php echo $this->NM_cancel_return_new ?>">
+<input type="hidden" name="csrf_token" value="<?php echo $this->scCsrfGetToken() ?>" />
+
+							<h2>
+								<?php echo $this->Ini->Nm_lang['lang_login_message_10']; ?>
+							</h2>
+						
+							<div class="control">
+								<label class="label" for="name"><?php echo $this->Ini->Nm_lang['lang_login_message_user']; ?></label>
+								<input class="input  sc-js-input " type="text" placeholder="<?php echo $this->Ini->Nm_lang['lang_login_user']; ?>"  name="login" id="id_sc_field_login" value="<?php echo $this->form_encode_input($login) ?>"  alt="{datatype: 'text', maxLength: 20, allowedChars: '<?php echo $this->allowedCharsCharset("") ?>', lettersCase: '', enterTab: false, enterSubmit: false, autoTab: false, selectOnFocus: true, watermark: '', watermarkClass: 'scFormObjectOddWm', maskChars: '(){}[].,;:-+/ '}" >
+							</div>
+
+							<div class="control">
+								<label class="label" for="pass"><?php echo $this->Ini->Nm_lang['lang_login_message_pswrd']; ?></label>
+								<div class="scFormObjectOddPwdBox">
+								<input class="input  sc-js-input " type="password" placeholder="<?php echo $this->Ini->Nm_lang['lang_login_pswrd']; ?>"  name="pswd" id="id_sc_field_pswd" value="<?php echo $this->form_encode_input($pswd) ?>"  alt="{datatype: 'text', maxLength: 20, allowedChars: '<?php echo $this->allowedCharsCharset("") ?>', lettersCase: '', enterTab: false, enterSubmit: false, autoTab: false, selectOnFocus: true, watermark: '', watermarkClass: 'scFormObjectOddWm', maskChars: '(){}[].,;:-+/ '}" >
+								<span id="id_pwd_show_hide_pswd" class="sc-ui-pwd-toggle-icon"><i class="fa fa-eye sc-ui-pwd-eye" aria-hidden="true" id="id_pwd_fa_pswd"></i></span></div>
+							</div>
+
+							<div class="submit">
+								<input class="button" type="submit"  onclick="nm_atualiza('alterar');"  value="<?php echo $this->Ini->Nm_lang['lang_page_login_button']; ?>" />
+							</div>
+						</form>
+						<!-- Idiomas -->
+						<div class="flag-lang">
+							<a class="flags flag-en" href="../login/?lang=en"><img src="../_lib/libraries/grp/healthcare/en_us.png" style="margin-top: -2px;padding-bottom: 0px;"></a>
+							<a class="flags flag-pt" href="../login/?lang=pt"><img src="../_lib/libraries/grp/healthcare/pt_br.png" style="margin-top: -2px;padding-bottom: 0px;"></a>
+							<a class="flags flag-es" href="../login/?lang=es"><img src="../_lib/libraries/grp/healthcare/es_es.png" style="margin-top: -2px;padding-bottom: 0px;"></a>
+						</div>
+						<hr style="margin: 25px 0 20px;">
+						<div id='message'>
+							<p style="margin: 0px;font-size: 12px;"><?php echo $this->Ini->Nm_lang['lang_page_login_login']; ?></p>
+							<br />
+							<p style="margin: 0px;font-size: 12px;"><?php echo $this->Ini->Nm_lang['lang_page_login_pswd']; ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="copyright">
+			<p><?php echo $this->Ini->Nm_lang['lang_page_copyright']; ?></p>
+		</div>
+	</body>	
+</html>
